ddfffdc9d59396d6ebacb0d999a51d9e268cb896
salman sattar <salmansattar01@gmail.com>
2024-04-06 22:03:19 +0500

    code edited

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 #! /usr/bin/env node
-export {};
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.9120521173,
+    INR: 93.3333333333,
+    PKR: 280,
+};
+let userAnswwer = await inquirer.prompt([
+    {
+        name: `from`,
+        message: `Enter From Currency`,
+        type: `list`,
+        choices: ["USD", `EUR`, `INR`, `PKR`],
+    },
+    {
+        name: `to`,
+        message: `Enter To Currency`,
+        type: `list`,
+        choices: ["USD", `EUR`, `INR`, `PKR`],
+    },
+    {
+        name: `amount`,
+        message: `Enter Your Amount`,
+        type: `number`,
+    },
+]);
+let fromAmount = currency[userAnswwer.from];
+let toAmount = currency[userAnswwer.to];
+let amount = userAnswwer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
--- index.ts
+++ index.ts
@@ -1,2 +1,37 @@
 #! /usr/bin/env node
-import inquirer from "inquirer"
+import inquirer from "inquirer";
+
+const currency:any = {
+  USD: 1,
+  EUR: 0.9120521173,
+  INR: 93.3333333333,
+  PKR: 280,
+};
+
+let userAnswwer:any = await inquirer.prompt([
+  {
+    name: `from`,
+    message: `Enter From Currency`,
+    type: `list`,
+    choices: ["USD", `EUR`, `INR`,`PKR`],
+  },
+  {
+    name: `to`,
+    message: `Enter To Currency`,
+    type: `list`,
+    choices: ["USD", `EUR`, `INR`,`PKR`],
+  },
+  {
+    name: `amount`,
+    message: `Enter Your Amount`,
+    type: `number`,
+  },
+])
+
+let fromAmount = currency[userAnswwer.from]
+let toAmount = currency[userAnswwer.to]
+let amount = userAnswwer.amount
+
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
